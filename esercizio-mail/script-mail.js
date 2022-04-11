23d7544f8bef744a1dafc93893376c7a5e79984f
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-04-11 15:17:57 +0200

    Risolto un bug

--- esercizio-mail/script-mail.js
+++ esercizio-mail/script-mail.js
@@ -1,4 +1,4 @@
-const listaAutorizzati = ['ciao@g.it'];
+const listaAutorizzati = ['asd@g.it','ciao@g.it','paolo@g.it'];
 
 
 
@@ -7,22 +7,26 @@ console.log(output)
 let verficaBtn = document.getElementById('bottone-verifica');
 
 verficaBtn.addEventListener('click', function(){
+  let mailUtente = document.getElementById('mail-check').value;
   for(i=0; i<listaAutorizzati.length; i++){
-    let mailUtente = document.getElementById('mail-check').value;
+    
     console.log(mailUtente);
     if(mailUtente == listaAutorizzati[i]){
       output.innerHTML = "L'utente puo' accedere";
       output.classList.remove('sfondo','yellow','red');
       output.classList.add('sfondo','green');
+      break;
     }else if(mailUtente==''){
       output.innerHTML = "Inserisci prima un indirizzo mail";
       output.classList.remove('sfondo','green','red');
       output.classList.add('sfondo','yellow');
+      
     }
     else{
       output.innerHTML = "L'utente non è abilitato ad eseguire l'accesso";
       output.classList.remove('sfondo','yellow','green');
       output.classList.add('sfondo','red');
+      
     }
   }
 })
